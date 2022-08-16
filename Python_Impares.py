0c315580fcf81548cce39913812c6fe76a75a32e
Rita Casiello <rituzka@gmail.com>
2022-08-16 21:41:49 +0200

    first commit

---
+++ Python_Impares.py
@@ -0,0 +1,10 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+for numero in range(2,8):
+    if numero % 2 != 0:
+        print(numero)
+
+        
+3
+5
+7
